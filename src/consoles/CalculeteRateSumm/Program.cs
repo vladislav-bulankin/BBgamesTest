b887c6a17e7b132f80469c1b40ecf12c76100055
Владислав Буланкин <schirif@mail.ru>
2025-02-05 18:02:49 +0300

    рефакторинг и фих бага 0го массива

--- src/consoles/CalculeteRateSumm/Program.cs
+++ src/consoles/CalculeteRateSumm/Program.cs
@@ -11,61 +11,27 @@ internal class Program {
         int[] arr = new int[q];
         Console.WriteLine("Введите суммы очков каждой из команд");
         for (int i = 0; i < arr.Length; i++) {
-            Console.WriteLine($"Сумма очков {i + 1} команды равна ");
-            Console.ReadLine();
+            Console.Write($"Сумма очков {i + 1} команды равна: ");
+            arr[i] = int.Parse(Console.ReadLine());
         }
-        int max1, max2, max3;
-        max1 = FindMax(arr);
-        int count = CountNumm(arr, max1);
-        if (count > 2) {
-            max3 = max2 = max1;
-        } else if (count > 1) {
-            max2 = max1;
-            max3 = FindNextMax(arr, max2);
-        } else {
-            max2 = FindNextMax(arr, max1);
-            count = CountNumm(arr, max2);
-            if (count > 1) {
+        int max1 = int.MinValue, max2 = int.MinValue, max3 = int.MinValue;
+
+        foreach (var score in arr) {
+            if (score > max1) {
                 max3 = max2;
-            } else {
-                max3 = FindNextMax(arr, max2);
+                max2 = max1;
+                max1 = score;
+            } else if (score > max2 && score < max1) {
+                max3 = max2;
+                max2 = score;
+            } else if (score > max3 && score < max2) {
+                max3 = score;
             }
         }
-        int sum = arr[max1] + arr[max2] + arr[max3];
+
+        int sum = max1 + max2 + max3;
         Console.WriteLine($"Сумма очков трёх команд, занявших первые три места равна {sum}");
         Console.Write("Нажмите любую клавишу");
         Console.ReadKey();
-    }
-
-    private static int FindNextMax (int[] arr, int max) {
-        int next = 0;
-        for (int i = 0; i < arr.Length; i++) {
-            if (arr[i] > arr[next] && arr[i] < arr[max]) {
-                next = i;
-            }else if (arr[next] == arr[max]) {
-                next = i;
-            }
-        }
-        return next;
-    }
-
-    private static int CountNumm (int[] arr, int numm) { 
-        int count = 0;
-        for (int i = 0; i < arr.Length; i++) {
-            if (arr[i] == arr[numm]) {
-                count++;
-            }
-        }
-        return count;
-    }
-
-    private static int FindMax (int[] arr) {
-        int x = 0;
-        for (int i = 0; i < arr.Length; i++) {
-            if(arr[i] > arr[x]) { 
-                x = i; 
-            }
-        }
-        return x;
     }
 }
